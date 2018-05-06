0fefe6068f416a2ba981a3580d422667073eb372
Ankit Gaurav <ankitgaurav@outlook.com>
2018-05-07 02:05:05 +0530

    Structure initial project

---
+++ src/components/RegisterView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <h1>Register</h1>
+      <input v-model="username" type="text" name="username" placeholder="username"/><br>
+      <input v-model="password" type="password" name="password" placeholder="password"/><br>
+      <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import authenticateService from '@/services/authenticateService';
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const res = await authenticateService.register({
+        username: this.username,
+        password: this.password
+      });
+      console.log(`Response from server: `, res);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
