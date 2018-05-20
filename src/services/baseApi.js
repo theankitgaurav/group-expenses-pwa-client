013f1403b40b295da8c187122d48d309e961944c
Ankit Gaurav <ankitgaurav@outlook.com>
2018-05-20 19:52:18 +0530

    Configure for deployment to firebase

--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default () => {
+    const isProd = process.env.NODE_ENV === 'production'
     return axios.create({
-        baseURL: `http://localhost:3000/`
+        baseURL: isProd? `https://group-expenses.herokuapp.com/`: `http://localhost:3000/`
     })
 }
