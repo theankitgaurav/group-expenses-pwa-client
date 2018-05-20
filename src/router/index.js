7f04e54378fa6456643e9c18448a85f4340f987c
Ankit Gaurav <ankitgaurav@outlook.com>
2018-05-21 01:45:44 +0530

    Replace meta-based auth middleware with navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Store from '@/store/store'
+import store from '@/store/store'
 import RegisterView from '@/components/RegisterView'
 import LoginView from '@/components/LoginView'
 import HomeView from '@/components/HomeView'
@@ -9,53 +9,73 @@ import authenticateService from '@/services/authenticateService'
 
 Vue.use(Router);
 
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isLoggedIn) {
+    next()
+    return
+  }
+  next('/home')
+}
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isLoggedIn) {
+    next()
+    return
+  }
+  next('/login')
+}
+
 const router = new Router({
   routes: [
+    {
+      path: '/',
+      name: 'index'
+    },
     {
       path: '/register',
       name: 'register',
       component: RegisterView,
-      meta: { requiresAuth: false }
+      beforeEnter: ifNotAuthenticated      
     },
     {
       path: '/login',
       name: 'login',
       component: LoginView,
-      meta: { requiresAuth: false }
+      beforeEnter: ifNotAuthenticated
     },
     {
       path: '/home',
       name: 'home',
       component: HomeView,
-      meta: { requiresAuth: true }
+      beforeEnter: ifAuthenticated
     },
     {
       path: '/new-entry',
       name: 'newEntry',
       component: PostEntryView,
-      meta: { requiresAuth: true }
+      beforeEnter: ifAuthenticated
     }
   ]
 });
 
 // Navigation guard to protect secure routes from 
 // un-authorized access
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
-    if (!Store.getters.isLoggedIn) {
-      authenticateService.logout();
-      next({
-        path: '/login',
-        query: { redirect: to.fullPath }
-      })
-    } else {
-      next()
-    }
-   } else {
-    next() // make sure to always call next()!
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some(record => record.meta.requiresAuth)) {
+//     // this route requires auth, check if logged in
+//     // if not, redirect to login page.
+//     if (!Store.getters.isLoggedIn) {
+//       authenticateService.logout();
+//       next({
+//         path: '/login',
+//         query: { redirect: to.fullPath }
+//       })
+//     } else {
+//       next()
+//     }
+//    } else {
+//     next() // make sure to always call next()!
+//   }
+// })
 
 export default router;
