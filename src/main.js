d9174b235670fe72f7b9652bda7bcb3a520b460b
Ankit Gaurav <ankitgaurav@outlook.com>
2018-05-21 11:31:04 +0530

    Modify vuetify resource imports

--- src/main.js
+++ src/main.js
@@ -3,15 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vuetify from 'vuetify'
 import {sync} from 'vuex-router-sync'
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vuetify from 'vuetify'
 import store from '@/store/store'
 
-
-Vue.use(Vuetify);
 Vue.config.productionTip = false;
 sync(store, router);
+Vue.use(Vuetify);
 
 /* eslint-disable no-new */
 new Vue({
