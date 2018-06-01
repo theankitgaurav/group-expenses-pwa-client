136940de4bcd65e59a93b5c312952ab1520a016b
Ankit Gaurav <ankitgaurav@outlook.com>
2018-06-01 20:32:18 +0530

    Import individual components  of vue-material instead of full bundle

--- src/main.js
+++ src/main.js
@@ -5,11 +5,36 @@ import App from './App'
 import router from './router'
 import {sync} from 'vuex-router-sync'
 import store from '@/store/store'
-import VueMaterial from 'vue-material'
+import { MdButton, MdContent, MdTabs, MdApp, MdAutocomplete, MdAvatar,
+  MdDatepicker, MdDivider, MdElevation, MdEmptyState, MdField, MdIcon,
+  MdImage, MdLayout, MdList, MdMenu, MdProgress, MdRipple, 
+  MdSnackbar, MdSubheader, MdToolbar, MdCard} from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
-Vue.use(VueMaterial)
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
+Vue.use(MdApp);
+Vue.use(MdAutocomplete);
+Vue.use(MdAvatar);
+Vue.use(MdDatepicker);
+Vue.use(MdDivider);
+Vue.use(MdElevation);
+Vue.use(MdEmptyState);
+Vue.use(MdField);
+Vue.use(MdIcon);
+Vue.use(MdImage);
+Vue.use(MdLayout);
+Vue.use(MdList);
+Vue.use(MdMenu);
+Vue.use(MdProgress);
+Vue.use(MdRipple);
+Vue.use(MdSnackbar);
+Vue.use(MdSubheader);
+Vue.use(MdToolbar);
+Vue.use(MdCard);
+
 Vue.config.productionTip = false;
 sync(store, router);
 
